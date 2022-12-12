dc0d837a9fab40dda07e0b4b0d51ad2a39a1c3a7
joelws <joel.w.simmons@gmail.com>
2022-12-12 09:58:36 -0600

    updated model names

--- planhat/models.py
+++ planhat/models.py
@@ -1,25 +1,25 @@
 '''Python Module that houses all model frameworks'''
 from planhat.api.framework import Model
 
-assets = Model(
+asset = Model(
     model_endpoint='assets',
     required_fields=['name','companyId'],
     model_docs='https://docs.planhat.com/#assets'
 )
 
-campaings = Model(
+campaign = Model(
     model_endpoint='campaings',
     required_fields=['name','companyId'],
     model_docs='https://docs.planhat.com/#campaigns'
 )
 
-churns = Model(
+churn = Model(
     model_endpoint='churn',
     required_fields=['companyId','value'],
     model_docs='https://docs.planhat.com/#churn'
 )
 
-companies = Model(
+company = Model(
     model_endpoint='companies',
     required_fields=['name'],
     model_docs='https://docs.planhat.com/#companies'
@@ -37,31 +37,31 @@ custom_fields = Model(
     model_docs='https://docs.planhat.com/#customfields'
 )
 
-end_users = Model(
+end_user = Model(
     model_endpoint='endusers',
     required_fields=['email','companyId'],
     model_docs='https://docs.planhat.com/#endusers'
 )
 
-invoices = Model(
+invoice = Model(
     model_endpoint='invoices',
     required_fields=['currency','cId'],
     model_docs='https://docs.planhat.com/#invoices'
 )
 
-issues = Model(
+issue = Model(
     model_endpoint='issues',
     required_fields=['title','sourceId','archived'],
     model_docs='https://docs.planhat.com/#issues'
 )
 
-licenses = Model(
+license = Model(
     model_endpoint='licenses',
     required_fields=['_currency','companyId','fromDate','value'],
     model_docs='https://docs.planhat.com/#licenses'
 )
 
-notes = Model(
+note = Model(
     model_endpoint='conversations',
     required_fields=['companyId','type'],
     model_docs='https://docs.planhat.com/#notes'
@@ -73,43 +73,43 @@ nps = Model(
     model_docs='https://docs.planhat.com/#nps'
 )
 
-opportunities = Model(
+opportuniy = Model(
     model_endpoint='opportunities',
     required_fields=['companyId'],
     model_docs='https://docs.planhat.com/#opportunities'
 )
 
-objectives = Model(
+objective = Model(
     model_endpoint='objectives',
     required_fields=['name','companyId'],
     model_docs='https://docs.planhat.com/#objectives'
 )
 
-projects = Model(
+project = Model(
     model_endpoint='projects',
     required_fields=['name','companyId','currency'],
     model_docs='https://docs.planhat.com/#projects'
 )
 
-sales = Model(
+sale = Model(
     model_endpoint='sales',
     required_fields=['_currency','companyId','salesDate','value'],
     model_docs='https://docs.planhat.com/#sales'
 )
 
-tasks = Model(
+task = Model(
     model_endpoint='tasks',
     required_fields=['mainType','companyId'],
     model_docs='https://docs.planhat.com/#tasks'
 )
 
-tickets = Model(
+ticket = Model(
     model_endpoint='tickets',
     required_fields=['comapnyId','sourceId','email'],
     model_docs='https://docs.planhat.com/#tickets'
 )
 
-users = Model(
+user = Model(
     model_endpoint='users',
     required_fields=['email','nickName','firstName','lastName'],
     model_docs='https://docs.planhat.com/#users'
